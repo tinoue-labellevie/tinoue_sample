87d74751622b43544408c2da59d8b5d96f955fb0
tinoue-labellevie <tinoue@labellevie.jp>
2025-04-15 15:01:52 +0900

    localStorageからwritableに変更

--- src/lib/stores/cart.ts
+++ src/lib/stores/cart.ts
@@ -1,4 +1,12 @@
 // src/lib/stores/cart.ts
 import { writable } from "svelte/store";
 
-export const cart = writable([]);
+export interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  imageUrl: string;
+}
+
+export const cart = writable<Product[]>([]);
